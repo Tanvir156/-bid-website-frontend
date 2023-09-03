e382d73529e0e3fc369bd0da20a972ed81b806cd
Tanvir156 <72537009+Tanvir156@users.noreply.github.com>
2023-09-04 04:51:01 +0600

    Update ProductCard.tsx

--- components/Home/ProductCard.tsx
+++ components/Home/ProductCard.tsx
@@ -27,8 +27,6 @@ export default function ProductCard({ product }) {
             src={product.image}
             alt={product.name}
             blurDataURL="data:..."
-            automatically
-            provided
             placeholder="blur"
           />
         </AspectRatio>
