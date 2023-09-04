ee10ae856acc5ca64728fed5e6a23e729dd8d993
Tanvir156 <72537009+Tanvir156@users.noreply.github.com>
2023-09-04 11:12:35 +0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "./../styles/globals.css";
-import Header from "./../components/Header/Header.tsx";
-import Footer from "./../components/Footer/Footer.tsx";
+import Header from "./../components/Header/Header";
+import Footer from "./../components/Footer/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./../styles/ProductDetailsCard.css";
 export default function App({ Component, pageProps }) {
