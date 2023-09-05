f484b041d7cffb2afe08058a2db6c6177107d0c0
web-DOT59 <webdot59@gmail.com>
2023-09-05 14:15:05 +0600

    uu

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { GetStaticPaths } from "next";
-import { useHistory } from "react-router-dom";
+
 import Registration from "./../components/Auth/Registration";
 const registration = ({ history }) => {
   return (
