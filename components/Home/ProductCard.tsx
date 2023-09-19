0ceaa33b8480866fb7d994a0829827a4e2eb00b7
tanvir/156 <rana525203@gmail.com>
2023-09-19 21:32:41 +0600

    under

--- components/Home/ProductCard.tsx
+++ components/Home/ProductCard.tsx
@@ -37,7 +37,7 @@ export default function ProductCard({ product }) {
         <Link
           href={`/product/${product._id}`}
           color="neutral"
-
+          style={{ textDecoration: "none" }}
           // endDecorator={<ArrowOutwardIcon />}
         >
           {product.title}
